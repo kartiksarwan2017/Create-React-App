d2a427bc78b396f85252ef0cc635cf3d34d13d1c
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-09-09 10:34:42 +0530

    Default Exports

--- DefaultExports/App.js
+++ DefaultExports/App.js
@@ -1,14 +1,23 @@
 /* Default Exports */
 
+// let a = 10;
+// var arr = [1, 2, 3, 4, 5];
+// const obj = {name: "Alexa"};
+
+// function greet() {
+//     console.log("Hello");
+// }
+
+// export default greet;
+// export {a, arr, obj};
+
+
 let a = 10;
 var arr = [1, 2, 3, 4, 5];
 const obj = {name: "Alexa"};
 
-function greet() {
+export default function greet() {
     console.log("Hello");
 }
 
-export default greet;
 export {a, arr, obj};
-
-
--- DefaultExports/index.js
+++ DefaultExports/index.js
@@ -1,7 +1,7 @@
-import greet from "./App.js";
+import greeting from "./App.js";
 import {a, arr, obj} from "./App.js";
 
 console.log(a);
 console.log(arr);
 console.log(obj);
-greet();
+greeting();
