44d8a0e1a56613e72f8f4f811705cd4b4a6592ee
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-09-09 10:33:01 +0530

    Default Exports

---
+++ DefaultExports/App.js
@@ -0,0 +1,14 @@
+/* Default Exports */
+
+let a = 10;
+var arr = [1, 2, 3, 4, 5];
+const obj = {name: "Alexa"};
+
+function greet() {
+    console.log("Hello");
+}
+
+export default greet;
+export {a, arr, obj};
+
+
---
+++ DefaultExports/index.js
@@ -0,0 +1,7 @@
+import greet from "./App.js";
+import {a, arr, obj} from "./App.js";
+
+console.log(a);
+console.log(arr);
+console.log(obj);
+greet();
